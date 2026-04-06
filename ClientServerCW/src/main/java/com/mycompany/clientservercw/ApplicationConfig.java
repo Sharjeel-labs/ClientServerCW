230f94d8310548ee4cca615303b583bc3f05f16a
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-06 21:46:53 +0100

    Added packages and classes

--- ClientServerCW/src/main/java/com/mycompany/clientservercw/ApplicationConfig.java
+++ ClientServerCW/src/main/java/com/mycompany/clientservercw/ApplicationConfig.java
@@ -1,19 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.clientservercw;
 
 /**
  *
  * @author mskha
  */
-public class ClientServerCW {
-
+public class ApplicationConfig {
     
-    public static void main(String[] args) {
-        
-        
-        System.out.println("Hello World!");
-    }
 }
---
+++ ClientServerCW/src/main/java/com/mycompany/exceptions/RoomNotEmptyException.java
@@ -0,0 +1,14 @@
+package com.mycompany.exceptions;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mskha
+ */
+public class RoomNotEmptyException {
+    
+}
