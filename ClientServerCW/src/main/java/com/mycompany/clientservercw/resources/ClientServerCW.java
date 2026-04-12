a647865bfe8c1b60782cbbb15f5b9fca65d48fd7
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-12 16:57:33 +0100

    Recreated project as Java Web Application with Tomcat

---
+++ ClientServerCW/src/main/java/com/mycompany/clientservercw/resources/ClientServerCW.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clientservercw.resources;
+
+/**
+ *
+ * @author mskha
+ */
+public class ClientServerCW {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello World!");
+    }
+}
