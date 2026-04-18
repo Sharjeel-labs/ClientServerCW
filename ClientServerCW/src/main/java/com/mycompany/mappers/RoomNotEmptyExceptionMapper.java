56cf88b7ffbbcddbe8f5af358977e9f1b97a7c8d
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-18 17:58:22 +0100

    mapper linked to execption

--- ClientServerCW/src/main/java/com/mycompany/mappers/RoomNotEmptyExceptionMapper.java
+++ ClientServerCW/src/main/java/com/mycompany/mappers/RoomNotEmptyExceptionMapper.java
@@ -4,10 +4,27 @@
  */
 package com.mycompany.mappers;
 
+import com.mycompany.exceptions.RoomNotEmptyException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author mskha
  */
-public class RoomNotEmptyExceptionMapper {
-    
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+
+    @Override
+    public Response toResponse(RoomNotEmptyException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+
+        return Response.status(Response.Status.CONFLICT) // 409
+                .entity(error)
+                .build();
+    }
 }
