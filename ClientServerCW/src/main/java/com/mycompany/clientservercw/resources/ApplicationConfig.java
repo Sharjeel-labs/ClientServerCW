c561b6c197d4a6d4d065ce3847defc4f9a8dd78e
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-19 19:17:08 +0100

    updated resouce and confiq file

--- ClientServerCW/src/main/java/com/mycompany/clientservercw/resources/ApplicationConfig.java
+++ ClientServerCW/src/main/java/com/mycompany/clientservercw/resources/ApplicationConfig.java
@@ -2,17 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package com.mycompany.clientservercw.resources;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  * @author mskha
  */
-//public class ApplicationConfig {
- @ApplicationPath("/api/v1")
-public class ApplicationConfig extends Application {
-}
 
+@ApplicationPath("/api/v1")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set<Class<?>> resources = new HashSet<>();
+
+        // register resources manually
+        resources.add(com.mycompany.resources.SensorRoomResource.class);
+        resources.add(com.mycompany.clientservercw.resources.JavaEE8Resource.class);
+
+        return resources;
+    }
+}
--- ClientServerCW/src/main/java/com/mycompany/resources/SensorRoomResource.java
+++ ClientServerCW/src/main/java/com/mycompany/resources/SensorRoomResource.java
@@ -2,13 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package com.mycompany.resources;
+
 import com.mycompany.models.Room;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author mskha
@@ -17,23 +20,19 @@ import java.util.List;
 @Path("/rooms")
 public class SensorRoomResource {
 
-    // simple in-memory list (no database)
     private static List<Room> rooms = new ArrayList<>();
 
-    // preloaded data (so GET isn't empty)
     static {
         rooms.add(new Room("LIB-301", "Library Room", 50));
         rooms.add(new Room("LAB-101", "Computer Lab", 30));
     }
 
-    // GET all rooms
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Room> getRooms() {
         return rooms;
     }
 
-    // GET room by ID
     @GET
     @Path("/{roomId}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -50,18 +49,13 @@ public class SensorRoomResource {
                 .build();
     }
 
-    // POST create room
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addRoom(Room room) {
         rooms.add(room);
-
-        return Response.status(Response.Status.CREATED)
-                .entity(room)
-                .build();
+        return Response.status(Response.Status.CREATED).entity(room).build();
     }
 
-    // DELETE room
     @DELETE
     @Path("/{roomId}")
     public Response deleteRoom(@PathParam("roomId") String roomId) {
