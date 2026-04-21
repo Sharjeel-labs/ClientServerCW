0256d7dd185948e516ea98c59f8d9dd095c687f3
Sharjeel-labs <w2092284@westminster.ac.uk>
2026-04-21 18:18:10 +0100

    updated

--- ClientServerCW/src/main/java/com/mycompany/mappers/LinkedResourceNotFoundExceptionMapper.java
+++ ClientServerCW/src/main/java/com/mycompany/mappers/LinkedResourceNotFoundExceptionMapper.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mappers;
+import com.mycompany.exceptions.LinkedResourceNotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 /**
  *
  * @author mskha
  */
-public class LinkedResourceNotFoundExceptionMapper {
-    
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException ex) {
+
+        return Response.status(Response.Status.BAD_REQUEST) // simpler than 422
+                .entity(ex.getMessage())
+                .build();
+    }
 }
